03104de591b8c229252a37012986a3d67ec49af1
Rendom-lex <140806354+Rendom-lex@users.noreply.github.com>
2023-09-13 17:28:59 +0300

    Add files via upload

---
+++ HW 60/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+class Program
+{
+    static void Main()
+    {
+        int[,,] array3D = GenerateRandom3DArray(2, 2, 2);
+        Print3DArrayWithIndices(array3D);
+    }
+    static int[,,] GenerateRandom3DArray(int xSize, int ySize, int zSize)
+    {
+        int[,,] array = new int[xSize, ySize, zSize];
+        Random rand = new Random();
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                for (int z = 0; z < zSize; z++)
+                {
+                    int randomNum;
+                    do
+                    {
+                        randomNum = rand.Next(10, 100);
+                    } while (IsNumberInArray(array, randomNum));
+                    array[x, y, z] = randomNum;
+                }
+            }
+        }
+        return array;
+    }
+    static bool IsNumberInArray(int[,,] array, int number)
+    {
+        foreach (int element in array)
+        {
+            if (element == number)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static void Print3DArrayWithIndices(int[,,] array)
+    {
+        int xSize = array.GetLength(0);
+        int ySize = array.GetLength(1);
+        int zSize = array.GetLength(2);
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                for (int z = 0; z < zSize; z++)
+                {
+                    Console.Write($"{array[x, y, z]}({x},{y},{z}) ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ HW 62/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 1 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+class Program
+{
+    static void Main()
+    {
+        int n = 4;
+        int[,] matrix = new int[n, n];
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        int counter = 1;
+        while (counter <= n * n)
+        {
+            for (int i = left; i <= right; i++)
+            {
+                matrix[top, i] = counter;
+                counter++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++)
+            {
+                matrix[i, right] = counter;
+                counter++;
+            }
+            right--;
+
+            for (int i = right; i >= left; i--)
+            {
+                matrix[bottom, i] = counter;
+                counter++;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--)
+            {
+                matrix[i, left] = counter;
+                counter++;
+            }
+            left++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{matrix[i, j]:D2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
